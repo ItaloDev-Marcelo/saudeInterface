374b522c71659d98501501feb990733c23b322f9
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-04-07 13:52:46 -0300

    feat:create loginForm component

--- src/components/form/loginForm.tsx
+++ src/components/form/loginForm.tsx
@@ -1,9 +1,30 @@
-// import React from 'react'
+import Input from "./utils/input"
+import {loginFormData} from './data/loginData'
+import { useForm, type SubmitHandler } from "react-hook-form"
 
-const loginForm = () => {
+
+type UseFormType = {
+  name: string,
+  password: string
+}
+
+const LoginForm = () => {
+
+  const {register,handleSubmit, formState: {errors}} = useForm<UseFormType>()
+
+  const onSubmit: SubmitHandler<UseFormType> = (data) => console.log(data)
+
   return (
-    <div>loginForm</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <Input type={loginFormData[0].type} errors={errors} 
+        title={loginFormData[0].title} name='name'
+        errorTxt={loginFormData[0].errorTxt} register={register} />
+        <Input type={loginFormData[1].type} errors={errors} 
+        title={loginFormData[1].title} name='password'
+        errorTxt={loginFormData[1].errorTxt} register={register} />
+        <button type='submit' className="bg-blue-400 w-[300px] h-9 rounded-sm text-white font-bold">Login</button>
+    </form>
   )
 }
 
-export default loginForm
+export default LoginForm
