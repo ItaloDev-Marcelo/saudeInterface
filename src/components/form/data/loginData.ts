7437aa176804c720e234f4ab25c472dc49c931d3
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-04-07 13:52:05 -0300

    Perf: create loginFormData

---
+++ src/components/form/data/loginData.ts
@@ -0,0 +1,15 @@
+export  const loginFormData = [
+    {
+        type: 'text',
+        errorTxt: 'this field is required',
+        title: 'Name',
+        name: 'name' 
+    },
+    {
+        type: 'password',
+        errorTxt: 'this field is required',
+        title: 'Password',
+        name: 'password' 
+    }
+]
+
