b890740bf9434b60b479b387f7aea10299da8c40
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-05-14 14:39:05 -0300

    perf:(Form):  use map to run my LoginData instead of repeat the same line

--- src/components/form/loginForm.tsx
+++ src/components/form/loginForm.tsx
@@ -1,28 +1,25 @@
-import Input from "./utils/input"
+import type { LoginFormType } from "../../types/LoginFormType"
+import Input from "./common/input"
 import {loginFormData} from './data/loginData'
 import { useForm, type SubmitHandler } from "react-hook-form"
 
 
-type UseFormType = {
-  name: string,
-  password: string
-}
 
 const LoginForm = () => {
 
-  const {register,handleSubmit, formState: {errors}} = useForm<UseFormType>()
+  const {register,handleSubmit, formState: {errors}} = useForm<LoginFormType>()
 
-  const onSubmit: SubmitHandler<UseFormType> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<LoginFormType> = (data) => console.log(data)
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-        <Input type={loginFormData[0].type} errors={errors} 
-        title={loginFormData[0].title} name='name'
-        errorTxt={loginFormData[0].errorTxt} register={register} />
-        <Input type={loginFormData[1].type} errors={errors} 
-        title={loginFormData[1].title} name='password'
-        errorTxt={loginFormData[1].errorTxt} register={register} />
-        <button type='submit' className="bg-blue-400 w-[300px] h-9 rounded-sm text-white font-bold">Login</button>
+       { loginFormData.map(item => (
+        <Input type={item.type} errors={errors} 
+        title={item.title} name={item.name} style='w-75'
+        errorTxt={item.errorTxt} register={register} />
+        ))
+       }
+        <button type='submit' className="bg-blue-400 w-[300px] h-9 rounded-sm text-white font-bold">Entrar</button>
     </form>
   )
 }
