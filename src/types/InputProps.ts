9732000fff27384a30c25b68cb507dfbaf89f235
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-05-14 14:49:29 -0300

    add type files to types folder

---
+++ src/types/InputProps.ts
@@ -0,0 +1,14 @@
+import type {FieldErrors, FieldValues, Path, UseFormRegister } from "react-hook-form"
+
+
+
+
+export type InputProps<T extends FieldValues> = {
+  register: UseFormRegister<T>,
+  name: Path<T>,
+  type: string,
+  errors: FieldErrors<T>
+  errorTxt: string,
+  title: string,
+  style: string
+}
