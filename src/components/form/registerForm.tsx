ad495dcd6566aed671286643e19f9e93e726248e
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-05-14 14:43:43 -0300

    reafactor(Form): use map to run my registerData  instead of repeat the same line

--- src/components/form/registerForm.tsx
+++ src/components/form/registerForm.tsx
@@ -1,9 +1,25 @@
-// import React from 'react'
+import type { RegisterFormType } from "../../types/RegisterFormType"
+import Input from "./common/input"
+import { registerFormData } from './data/registerData'
+import { useForm, type SubmitHandler } from "react-hook-form"
 
-const registerForm = () => {
+
+const RegisterForm = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<RegisterFormType>()
+  const onSubmit: SubmitHandler<RegisterFormType> = (data) => console.log(data)
+
+
   return (
-    <div>registerForm</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {registerFormData.map(item => (
+        <Input type={item.type} errors={errors}
+          title={item.title} name={item.name} style='w-100'
+          errorTxt={item.errorTxt} register={register} />
+      ))
+      }
+      <button type='submit' className="bg-blue-400 mt-4 w-[400px] h-12 rounded-sm text-white font-bold">Enviar</button>
+    </form>
   )
 }
 
-export default registerForm
+export default RegisterForm
