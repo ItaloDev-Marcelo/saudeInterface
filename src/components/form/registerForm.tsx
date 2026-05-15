4236d1547970b575a8ae3bbffc282bd1c261c169
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-05-15 14:54:17 -0300

    feat: add sendData and axios to registerForm component and add hundleNextForm

--- src/components/form/registerForm.tsx
+++ src/components/form/registerForm.tsx
@@ -1,23 +1,49 @@
 import type { RegisterFormType } from "../../types/RegisterFormType"
+import axios from 'axios'
 import Input from "./common/input"
 import { registerFormData } from './data/registerData'
 import { useForm, type SubmitHandler } from "react-hook-form"
 
 
-const RegisterForm = () => {
+type ResgisterPropsType = {
+  hundleNextForm : (next:boolean) => void
+}
+
+const RegisterForm = ({hundleNextForm}:ResgisterPropsType) => {
+
   const { register, handleSubmit, formState: { errors } } = useForm<RegisterFormType>()
-  const onSubmit: SubmitHandler<RegisterFormType> = (data) => console.log(data)
+  
+  const onSubmit: SubmitHandler<RegisterFormType> = (data) => {
+        const {username,email,password} = data;
+        sendData(username,email,password)
+  }
+
+
+  const sendData = async (username:string,email:string,password:string) => {
+        try {
+          const result =  await axios.post('https://api-brasilsaude.onrender.com/api/auth/register', {
+          name: username,
+          email,
+          password
+        })
+        console.log(result.data)
+        const {success} = result.data
+        hundleNextForm(success)
+        }catch(error) {
+          console.log(error)
+        }
+  }
 
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {registerFormData.map(item => (
+       {registerFormData.map(item => (
         <Input type={item.type} errors={errors}
           title={item.title} name={item.name} style='w-100'
           errorTxt={item.errorTxt} register={register} />
       ))
       }
-      <button type='submit' className="bg-blue-400 mt-4 w-[400px] h-12 rounded-sm text-white font-bold">Enviar</button>
+      <button type='submit' className="bg-blue-400 mt-4 w-100 h-12 rounded-sm text-white font-bold">Enviar</button>
     </form>
   )
 }
