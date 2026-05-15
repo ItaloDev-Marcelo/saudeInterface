7e238f47ab3691185f33ec2d407ecf7340533b8e
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-05-15 15:09:09 -0300

    style: change backgroundImage from tailwindcss to style

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import LoginForm from './components/form/loginForm'
 import './App.css'
 import RegisterForm from './components/form/registerForm'
 import { useState } from 'react'
-
+import banner from './assets/banner.jpg'
 function App() {
   const [nextForm, setNextForm] = useState(true);
   const [popUp, setPopUp] = useState(false)
   const hundleNextForm = (nextValue: boolean) => setNextForm(nextValue)
   const hundlePopUp = (nextValue: boolean) => setPopUp(nextValue)
   return (
-    <div className='w-screen bg-cover flex flex-col items-center justify-center h-screen bg-[url(assets/banner.jpg)] '>
+    <div 
+     style={{
+    backgroundImage: `url(${banner})`
+  }}
+    className='w-screen bg-cover flex flex-col items-center justify-center h-screen '>
 
      {
       !nextForm ? <CommunLayout style='w-[500px] h-[500px] p-2 flex flex-col text-white justify-center items-center'>
@@ -25,7 +29,7 @@ function App() {
 
        {
         nextForm && popUp === false ? <CommunLayout style='w-[500px] h-[350px] p-2 flex flex-col text-white justify-center items-center'>
-             <h1 className='text-2xl font-semibold mb-2'>Login</h1>
+             <h1 className='text-2xl font-semibold mb-1'>Login</h1>
             <LoginForm hundlePopUp={hundlePopUp}/>
         </CommunLayout> : null
        }
