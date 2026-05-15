f00a03ab650b2fb592b215cd43214fa56874bdf1
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-05-15 15:09:45 -0300

    feat: create SubmitBtn component

---
+++ src/components/form/common/SubmitBtn.tsx
@@ -0,0 +1,8 @@
+
+const SubmitBtn = () => {
+  return (
+    <button type='submit' className="bg-blue-400 hover:bg-blue-600 cursor-pointer w-75 h-9 rounded-sm text-white font-bold">Entrar</button>
+  )
+}
+
+export default SubmitBtn
