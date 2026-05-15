210842d6563ed80c4f1602d2d4b9a331012c5d93
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-05-15 14:49:18 -0300

    feat: add sendData function and add handlePopUp to LoginForm

--- src/components/form/loginForm.tsx
+++ src/components/form/loginForm.tsx
@@ -1,15 +1,35 @@
 import type { LoginFormType } from "../../types/LoginFormType"
 import Input from "./common/input"
+import axios from 'axios';
 import {loginFormData} from './data/loginData'
 import { useForm, type SubmitHandler } from "react-hook-form"
 
+type loginFormProp = {
+  hundlePopUp : (next:boolean) => void
+}
 
+const LoginForm = ({hundlePopUp}:loginFormProp) => {
 
-const LoginForm = () => {
+  const {register,handleSubmit, formState: {errors}} = useForm<LoginFormType>();
 
-  const {register,handleSubmit, formState: {errors}} = useForm<LoginFormType>()
-
-  const onSubmit: SubmitHandler<LoginFormType> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<LoginFormType> = (data) => {
+    const {email,password} = data;
+    sendData(email,password)
+  }
+  
+    const sendData = async (email:string,password:string) => {
+          try {
+            const result =  await axios.post('https://api-brasilsaude.onrender.com/api/auth/login', {
+            email,
+            password
+          })
+          console.log(result.data)
+          hundlePopUp(result.data.success)
+          }catch(error) {
+            console.log(error)
+          }
+    }
+ 
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -19,7 +39,7 @@ const LoginForm = () => {
         errorTxt={item.errorTxt} register={register} />
         ))
        }
-        <button type='submit' className="bg-blue-400 w-[300px] h-9 rounded-sm text-white font-bold">Entrar</button>
+        <button type='submit' className="bg-blue-400 w-75 h-9 rounded-sm text-white font-bold">Entrar</button>
     </form>
   )
 }
